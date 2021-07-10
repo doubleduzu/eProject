019cdd3877c08a7d23f72bc5931e8ccd6197600b
Henlla <phamkiet2234@gmail.com>
2021-07-10 20:57:20 +0700

    a

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
 // Show methods
 function button(c) {
-  if (c == 1) {
-    document.getElementById('method1').style.display = 'block';
-    document.getElementById('method2').style.display = 'none';
-  } else {
-    document.getElementById('method2').style.display = 'block';
-    document.getElementById('method1').style.display = 'none';
-  }
+    if (c == 1) {
+        document.getElementById('method1').style.display = 'block';
+        document.getElementById('method2').style.display = 'none';
+    } else {
+        document.getElementById('method2').style.display = 'block';
+        document.getElementById('method1').style.display = 'none';
+    }
 }
 // End show methods
 
 // Scroll top button
 var topButton = document.getElementById('scroll-top');
-window.onscroll = function () {
-  scrollFunction();
+window.onscroll = function() {
+    scrollFunction();
 };
+
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    topButton.style.display = 'block';
-  } else {
-    topButton.style.display = 'none';
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = 'block';
+    } else {
+        topButton.style.display = 'none';
+    }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 // End scroll top button
