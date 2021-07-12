d58d3aee1c39eb6d961ed406b9a9bd34749f86b4
Henlla <phamkiet2234@gmail.com>
2021-07-12 21:26:10 +0700

    k

--- js/Education.js
+++ js/Education.js
@@ -6,12 +6,6 @@ function buyNow() {
         alert("Please fill your information");
         return
     }
-    var patt_name = /^[A-Z]\w{4,}$/
-    var result = patt_name.test(fullName);
-    if (result == false) {
-        alert("Uppercase first character and at least 5 character");
-        return
-    }
     var patt_email = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
     result = patt_email.test(email);
     if (result == false) {
