4c93fbd43ae2aa62b567838d14b8d8f8da8a8181
Henlla <phamkiet2234@gmail.com>
2021-07-12 13:30:23 +0700

    k

--- js/Education.js
+++ js/Education.js
@@ -4,12 +4,6 @@ function buyNow() {
     var phone = document.getElementById("phone").value;
     if (fullName == "" || email == "" || phone == "") {
         alert("Please fill your information");
-        return;
-    }
-    var patt_email = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
-    var mail = patt_email.test("email")
-    if (mail == true) {
-        alert("Wrong type email");
         return
     }
     var choice = confirm("Information" + "\nName: " + fullName + "\nEmail: " + email + "\nPhone: " + phone + "\n\nAre you sure to buy this course ?");
