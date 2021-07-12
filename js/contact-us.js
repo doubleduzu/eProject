9f95917724c9775ff614c5b3e76f91669b4c137b
Henlla <phamkiet2234@gmail.com>
2021-07-12 14:43:11 +0700

    k

--- js/contact-us.js
+++ js/contact-us.js
@@ -2,7 +2,6 @@ function contact() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
-
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("message").value;
     if (name == '' || email == '' || phone == '' || subject == '' || message == '') {
@@ -13,5 +12,8 @@ function contact() {
         alert("Phone must be a number");
         return
     }
-    alert("Information" + "\nName: " + name + "\nEmail: " + email + "\nPhone: " + phone + "\nSubject: " + subject + "\nMessage: " + message);
+    var choice = confirm("Information" + "\nName: " + name + "\nEmail: " + email + "\nPhone: " + phone + "\nSubject: " + subject + "\nMessage: " + message)
+    if (choice == 1) {
+        alert("Your Information was sent");
+    }
 }
