e7c51a55f9e6c02b7273d4e232115c01198a9997
Henlla <phamkiet2234@gmail.com>
2021-07-12 13:21:07 +0700

    k

--- js/Education.js
+++ js/Education.js
@@ -1,16 +1,16 @@
 function buyNow() {
     var fullName = document.getElementById("fullName").value;
     var email = document.getElementById("mail").value;
+    var phone = document.getElementById("phone").value;
+    if (fullName == "" || email == "" || phone == "") {
+        alert("Please fill your information");
+        return;
+    }
     var patt_email = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
     var mail = patt_email.test("email")
     if (mail == true) {
         alert("Wrong type email");
         return
-    }
-    var phone = document.getElementById("phone").value;
-    if (fullName == "" || email == "" || phone == "") {
-        alert("Please fill your information");
-        return;
     }
     var choice = confirm("Information" + "\nName: " + fullName + "\nEmail: " + email + "\nPhone: " + phone + "\n\nAre you sure to buy this course ?");
     if (choice == 1) {
