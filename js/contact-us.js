fe4bd8f0a6c4cf39b6dd687b6fc2813707634295
Henlla <phamkiet2234@gmail.com>
2021-07-12 16:07:48 +0700

    k

--- js/contact-us.js
+++ js/contact-us.js
@@ -8,7 +8,7 @@ function contact() {
         alert("Please fill all your information");
         return;
     }
-    var patt_name = /^[A-Z]\w{5,}$/
+    var patt_name = /^[A-Z]\w{4,}$/
     var result = patt_name.test(name);
     if (result == false) {
         alert("Uppercase first character and at least 5 character");
