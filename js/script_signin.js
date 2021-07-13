7bf250aff0783554e968f2870f7c4638bf597d12
Henlla <phamkiet2234@gmail.com>
2021-07-13 10:11:34 +0700

    k

--- js/script_signin.js
+++ js/script_signin.js
@@ -1,16 +1,16 @@
-function toggleResetPswd(e){
+function toggleResetPswd(e) {
     e.preventDefault();
     $('#logreg-forms .form-signin').toggle() // display:block or none
     $('#logreg-forms .form-reset').toggle() // display:block or none
 }
 
-function toggleSignUp(e){
+function toggleSignUp(e) {
     e.preventDefault();
     $('#logreg-forms .form-signin').toggle(); // display:block or none
     $('#logreg-forms .form-signup').toggle(); // display:block or none
 }
 
-$(()=>{
+$(() => {
     // Login Register Form
     $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
     $('#logreg-forms #cancel_reset').click(toggleResetPswd);
@@ -18,23 +18,15 @@ $(()=>{
     $('#logreg-forms #cancel_signup').click(toggleSignUp);
 })
 
-
-function changeToHomePage() {
-    alert("run")
-    //location.replace("https://doubleduzu.github.io/eProject/index.html");
-    //window.location.replace("https://doubleduzu.github.io/eProject/index.html");
-    window.location.href = "https://doubleduzu.github.io/eProject/index.html";
-}
-
 function signIn() {
     var iemail = document.getElementById("inputEmail").value;
     var ipwd = document.getElementById("inputPassword").value;
-    
-    if(iemail == '' || ipwd == '') {
+
+    if (iemail == '' || ipwd == '') {
         alert("Please fill all your information");
         return;
     }
-    
+
     var patt_iemail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
     const result = patt_iemail.test(String(iemail).toLowerCase());
     if (result == false) {
@@ -42,13 +34,7 @@ function signIn() {
         document.getElementById("inputEmail").focus();
         return;
     }
-
-    
-    var choice = window.confirm("Check Information: \n-------------------------------" + "\nEmail: " + iemail + "\nPassword: " + ipwd + "\n-------------------------------\n\nYour information was sent!!!");  
-    if (choice == true) {
-        window.location.href = "https://doubleduzu.github.io/eProject/index.html";
-    }
-    changeToHomePage();
+    alert("Login Successful")
 }
 
 function signUp() {
@@ -57,11 +43,14 @@ function signUp() {
     var upass = document.getElementById("user-pass").value;
     var urepass = document.getElementById("user-repeatpass").value;
 
-    if(uname == '' || umail == '' || upass == '' || urepass == '') {
+    if (uname == '' || umail == '' || upass == '' || urepass == '') {
         alert("Please fill all your information");
         return;
     }
-
+    if (upass != urepass) {
+        alert("Your pass and repass not same");
+        return;
+    }
     var patt_iemail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
     const result = patt_iemail.test(String(umail).toLowerCase());
     if (result == false) {
@@ -69,16 +58,7 @@ function signUp() {
         document.getElementById("inputEmail").focus();
         return;
     }
-
-    var choice = window.confirm("Check Information: \n-------------------------------" 
-    + "\nFull name: " + uname 
-    + "\nEmail address: " + umail 
-    + "\nPassword: " + upass 
-    + "\nRepeat Password: " + urepass 
-    + "\n-------------------------------\n\nYour information was sent!!!");  
-    if (choice == true) {
-        window.location.href = "signin.html";
-    }
+    alert("Sign Up Successful")
 }
 
 function resetPassword() {
@@ -86,7 +66,3 @@ function resetPassword() {
     alert("Your email is " + remail + "\n\nNew password was sent to your email " + "\nPlease check your mail!!");
     window.location.href = "siginin.html";
 }
-
-
-
-
